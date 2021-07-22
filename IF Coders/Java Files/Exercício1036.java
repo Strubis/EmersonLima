23c83e8114c85ea263ce11afbe7322ef210f0f61
Emerson Lima <68118899+Strubis@users.noreply.github.com>
2021-07-22 14:44:22 -0300

    Update Exercício 1036

--- IF Coders/Java Files/Exercício1036.java
+++ IF Coders/Java Files/Exercício1036.java
@@ -11,6 +11,7 @@ public class Exercício1036 {
         // Criação das variáveis.
         double a, b, c;
         
+        // Criação do leitor
         Scanner tec = new Scanner(System.in);
         
         // Leitura dos valores.
@@ -24,16 +25,20 @@ public class Exercício1036 {
     }
     
     private static void formBhaskara(double a, double b, double c){
+        // Cálculo do Delta
         double delta = Math.pow(b, 2) - ( 4 * a * c );
         
+        // Cálculo das raízes
         double xPos = ( -b + Math.sqrt( delta ) ) / (2 * a);
         double xNeg = ( -b - Math.sqrt( delta ) ) / (2 * a);
         
+        // Valor negativo, logo não tem raíz
         if( ( 2 * a ) == 0 || delta < 0 ){
             System.out.println("Impossivel calcular");
             return;
         }
-            
+        
+        // Raízes existentes
         System.out.printf( "R1 = %.5f\n", xPos );
         System.out.printf( "R2 = %.5f\n", xNeg );
         
